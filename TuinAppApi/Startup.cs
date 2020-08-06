1d79b7d45e561dabc029cc57e31490e29d01e321
GlennBeeckman <GlennBeeckman@users.noreply.github.com>
2020-08-06 13:37:31 +0200

    added secret

--- TuinAppApi/Startup.cs
+++ TuinAppApi/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using TuinAppApi.Data;
 using TuinAppApi.Data.Repositories;
 using TuinAppApi.Models;
@@ -79,6 +82,13 @@ namespace TuinAppApi
             //services.AddSwaggerDocument();
 
             services.AddCors(options => options.AddPolicy("AllowAllOrigins", builder => builder.AllowAnyOrigin()));
+
+            services.AddAuthentication(x => { x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; })
+                .AddJwtBearer(x => {
+                    x.SaveToken = true; x.TokenValidationParameters = new TokenValidationParameters { 
+                        ValidateIssuerSigningKey = true, IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(Configuration["Tokens:Key"]))
+                        , ValidateIssuer = false, ValidateAudience = false, RequireExpirationTime = true }; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -98,6 +108,8 @@ namespace TuinAppApi
             app.UseCors("AllowAllOrigins");
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
